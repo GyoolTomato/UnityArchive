2c7c7354b9fd873ba7a87a092c2de2dbac2e4a01
RohTaeyoung <gtmt0127@outlook.com>
2024-06-02 18:40:38 +0900

    Create

---
+++ Assets/Scripts/_Common/Com_Base.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Com_Base : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    protected virtual void Tick()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected virtual void Tick_Sec()
+    {
+
+    }
+}
---
+++ Assets/Scripts/_Common/Panel_Base.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Panel_Base : MonoBehaviour
+{
+ 
+    /// <summary>
+    /// 
+    /// </summary>
+    protected virtual void Tick()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected virtual void Tick_Sec()
+    {
+
+    }
+}
